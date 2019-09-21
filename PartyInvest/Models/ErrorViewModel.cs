9c0de864c736caf8769a62ce5d8cd2a4688ac595
bodysafff <bodyasafff@gmail.com>
2019-09-21 16:13:00 +0300

    Add project files.

---
+++ PartyInvest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PartyInvest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
